c8ebc2ec4ec511592ed91cfd768f03a9d5dc7d39
mlobf <marcoslemeborbafilho@gmail.com>
2022-05-23 22:54:22 -0300

    Gzip to compress data

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const cheerio = require("cheerio");
 const URL = "https://www.imdb.com/title/tt0102926/";
 
 (async () => {
-  //const response = await request(URL);
-
   const response = await request({
     uri: URL,
     headers: {
-      "User-Agent": "Request-Promise",
+      Accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "Accept-encoding": "gzip, deflate, br",
+      "Accept-language": "en-US,en;q=0.9",
+      "Cache-control": "no-cache",
+      "Upgrade-insecure-requests": "1",
+      "User-agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36",
     },
+    gzip: true,
   });
 
   let $ = cheerio.load(response);
-  // class sc-94726ce4-1 iNShGo
   let title = $('div[class="sc-94726ce4-1 iNShGo"] > h1').text();
-  console.log(title);
   let rate = $('span[class="sc-7ab21ed2-1 jGRxWM"]').text();
-  console.log(rate);
+  console.log(title, rate);
 })();
---
+++ json.js
@@ -0,0 +1,9 @@
+const headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-encoding': 'gzip, deflate, br',
+    'Accept-language': 'en-US,en;q=0.9',
+    'Cache-control': 'no-cache',
+    'Cookie': 'session-id-time=2082787201l; session-id=133-8367844-6445013; csm-hit=tb:D2VHZ54KZMBZ038BBHM6+s-D2VHZ54KZMBZ038BBHM6|1653351608393&t:1653351608393&adb:adblk_no',
+    'Upgrade-insecure-requests': '1',
+    'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36',
+}
